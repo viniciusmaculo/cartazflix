b3d17e88bcb24f0868204b8b238f29bffdbd30f0
Vinícius Maçulo <vinimaculo@gmail.com>
2023-11-18 10:24:22 -0300

    ajusta homepage

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './reset.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Router basename="/cartazflix"></Router>
     <App />
   </React.StrictMode>
 );
