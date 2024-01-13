0a90d27590c5bfa59aba691e555b325c5f1ec94c
Vinícius Maçulo <vinimaculo@gmail.com>
2024-01-12 22:41:31 -0300

    Atualiza rotas

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Header() {
     return(
         <header>
-            <Link to={"/cartazflix"} className='logo'><h1>Cartaz Flix</h1></Link>
+            <Link to={"/"} className='logo'><h1>Cartaz Flix</h1></Link>
             <Link to={"/favorites"} className='favorites'><button>Meus Filmes</button></Link>
         </header>
 
